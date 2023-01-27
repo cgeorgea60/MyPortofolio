088d5c712637f2f989298b06a2d7c1983d228da9
cgeorgea60 <64281418+cgeorgea60@users.noreply.github.com>
2023-01-27 19:26:59 +0100

    second commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
+import Navbar from "./components/Navbar";
+
 function App() {
   return (
-    <div className="text-center">
+    <div className="text-center h-screen justify-center items-center w-full bg-red-400">
+      <Navbar />
       <h1 className="text-6xl">App page</h1>
     </div>
   );
