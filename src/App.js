bae549f5e0a547184ec76b16513a9c55a39b8614
cgeorgea60 <64281418+cgeorgea60@users.noreply.github.com>
2023-01-27 20:53:21 +0100

    navbar first commit

--- src/App.js
+++ src/App.js
@@ -3,10 +3,10 @@ import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    <div className="text-center h-screen justify-center items-center w-full bg-red-400">
+    <div className="text-center h-screen justify-center items-center w-full bg-black">
       <Navbar/>
       <Banner/>
-      <h1 className="text-6xl">App page</h1>
+      <h1 className="text-6xl text-white">App page</h1>
 
     </div>
   );
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,14 +4,14 @@ import { AiOutlineMenu } from 'react-icons/ai';
 const Navbar = () => {
   return (
     <div className="w-full flex justify-center h-24">
-      <div className="text-center flex justify-between items-center px-2 w-full bg-blue-600">
+      <div className="text-center flex justify-between items-center px-2 w-full md:min-w-[80%] bg-black text-white">
         <div>
-          <p>
-            Portfo<span className="text-red-500">lio</span>
+          <p className="text-xl">
+            Portfo<span className="text-pink-700 cursor-pointer">lio</span>
           </p>
         </div>
         <div className="hidden  md:block">
-      <div className="flex space-x-8">
+      <div className="flex space-x-8 cursor-pointer">
           <p>Home</p>
           <p>About Us</p>
           <p>Services</p>
@@ -21,7 +21,7 @@ const Navbar = () => {
       </div>
 
         </div>
-        <div className="flex">Download CV</div>
+        <div className="flex rounded-full bg-pink-700 p-2  px-3 cursor-pointer md:px-4 text-sm">Download CV</div>
         <div className="md:hidden">
         <AiOutlineMenu className="text-4xl cursor-pointer hover:scale-110"/>
         </div>
